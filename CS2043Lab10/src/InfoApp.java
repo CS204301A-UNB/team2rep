38138e9dc76fe68c4afbc565cfd1195aa19007f8
dwood2265 <dylan.swimwood@gmail.com>
2015-12-01 17:22:14 -0400

    Wood full name

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -1,9 +1,12 @@
 
 public class InfoApp {
+	
 
 	public static void main(String[] args) {
-
+		InfoPrintWood wood = new InfoPrintWood();
+		
 		System.out.println("Team 2 Member list:");
+		wood.printFullName();
 	}
 
 }
---
+++ CS2043Lab10/src/InfoPrintWood.java
@@ -0,0 +1,6 @@
+
+public class InfoPrintWood {
+	public void printFullName(){
+		System.out.println("Dylan Wood");
+	}
+}
