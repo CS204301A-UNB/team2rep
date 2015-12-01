6e45f68c1a9c393690474a207b2b4ae11f715428
HannahMeneley <hannah.meneley@unb.ca>
2015-12-01 17:20:59 -0400

    Updated To Hannah

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -4,6 +4,8 @@ public class InfoApp {
 	public static void main(String[] args) {
 
 		System.out.println("Team 2 Member list:");
+		InforPrintMeneley meneley = new InforPrintMeneley();
+		meneley.printMeneley();
 	}
 
 }
