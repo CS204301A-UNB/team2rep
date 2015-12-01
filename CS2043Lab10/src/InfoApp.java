489467f1b379ec254b8dbe6f190e73fa4ec9ee1d
clogan2 <clogan2@unb.edu>
2015-12-01 18:22:15 -0400

    Here!

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -4,6 +4,8 @@ public class InfoApp {
 	public static void main(String[] args) {
 
 		System.out.println("Team 2 Member list:");
+		InfoPrintLogan logan = new InfoPrintLogan();
+		logan.printLogan();
 		InforPrintMeneley meneley = new InforPrintMeneley();
 		meneley.printMeneley();
 		InfoPrintWestfield west = new InfoPrintWestfield();
