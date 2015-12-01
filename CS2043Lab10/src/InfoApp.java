cec9733fa3570a1239a411869d78da3f444410b5
clogan2 <clogan2@unb.edu>
2015-12-01 17:28:46 -0400

    Here ya go!

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -4,6 +4,8 @@ public class InfoApp {
 	public static void main(String[] args) {
 
 		System.out.println("Team 2 Member list:");
+		InfoPrintLogan printLogan = new InfoPrintLogan();
+		printLogan.printLogan();
 	}
 
 }
---
+++ CS2043Lab10/src/InfoPrintLogan.java
@@ -0,0 +1,6 @@
+
+public class InfoPrintLogan {
+	public void printLogan() {
+		System.out.println("Christopher Grant Logan");
+	}
+}
